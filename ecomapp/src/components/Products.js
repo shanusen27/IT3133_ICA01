0859e14f750de224ca744ebee54087a004e1165e
shanusen27 <139607388+shanusen27@users.noreply.github.com>
2024-12-03 15:57:09 +0000

    Update products grid

--- ecomapp/src/components/Products.js
+++ ecomapp/src/components/Products.js
@@ -1,29 +1,36 @@
+import { useState } from "react";
+import "../assets/CSS/layout.css";
+import Cart from "./Cart";
+import { flowers } from "./FlowerDB";
+import Product from "./Product";
 
-import '../assets/CSS/layout.css';
+export default function Products() {
+  const [cart, setCart] = useState([]);
+  const [total, setTotal] = useState(0);
 
-
-export default function Products(){
-   
-    return(
-        <>
-            <div className="item1">
-                <h1>Flower Shop</h1>
-            </div>
-            <div className="item2">
-                <h4 className="card-title">Buy flowers</h4>
-                <div className="grid-container">
-                    {
-                        //product
-                    }
-                </div>
-
-            </div>
-            <div className="item3">
-                {
-                //cart
-                }
-            </div>
-        </>
-    );
-
+  return (
+    <>
+      <div className="item1">
+        <h1>Flower Shop</h1>
+      </div>
+      <div className="item2">
+        <h4 className="card-title">Buy flowers</h4>
+        <div className="grid-container">
+          {flowers.map((flower, index) => {
+            return (
+              <Product
+                key={index}
+                product={flower}
+                setCart={setCart}
+                setTotal={setTotal}
+              />
+            );
+          })}
+        </div>
+      </div>
+      <div className="item3">
+        <Cart cart={cart} total={total} />
+      </div>
+    </>
+  );
 }
