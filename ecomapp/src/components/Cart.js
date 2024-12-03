462aa2ebe52047087bd4865eb93a31603cc783f7
shanusen27 <139607388+shanusen27@users.noreply.github.com>
2024-12-03 15:50:39 +0000

    Display items and total in cart

--- ecomapp/src/components/Cart.js
+++ ecomapp/src/components/Cart.js
@@ -1,25 +1,27 @@
-export default function Cart() {
-    
+export default function Cart({ cart, total }) {
     return (
-        <div className="table-container">
-            <h4 className="card-title">Cart</h4>
-            <table>
-                <thead>
-
-                    <td>Product</td>
-                    <td>QTY</td>
-                    <td>Price</td>
-
-                </thead>
-                {
-                    
-                }
-                <tr>
-                    <td >Grand Total : </td>
-                    <td colSpan={2}>Grand Total</td>
-                </tr>
-            </table>
-           
-        </div>
+      <div className="table-container">
+        <h4 className="card-title">Cart</h4>
+        <table>
+          <thead>
+            <td>Product</td>
+            <td>QTY</td>
+            <td>Price</td>
+          </thead>
+          {cart.map((item, index) => {
+            return (
+              <tr key={index}>
+                <td>{item.product}</td>
+                <td>{item.quantity}</td>
+                <td>{item.price}</td>
+              </tr>
+            );
+          })}
+          <tr>
+            <td>Grand Total : </td>
+            <td colSpan={2}>{total}</td>
+          </tr>
+        </table>
+      </div>
     );
-}
+  }
