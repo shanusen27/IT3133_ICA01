1452373aae6a9ffd2293683a000bab8ca7120e0e
shanusen27 <139607388+shanusen27@users.noreply.github.com>
2024-12-03 15:55:13 +0000

    Display items and total in cart

--- ecomapp/src/components/Product.js
+++ ecomapp/src/components/Product.js
@@ -1,20 +1,50 @@
-import '../assets/CSS/layout.css';
-export default function Product(){
-    
-    return(
-        <div className="grid-item">
+import { useState } from "react";
+import "../assets/CSS/layout.css";
 
-            <div class="card">
-                <img  />
-                <div class="card-body">
-                    <h5 class="card-title">Price:</h5>
-                    <div class="quantity-container">
-                        <label for="quantity">Quantity:</label>
-                        <input type="number" id="quantity" name="quantity" />
-                    </div>
-                    <button class="card-button">Add to Cart</button>
-                </div>
-            </div>
+export default function Product({ key, product, setCart, setTotal }) {
+  const [quantity, setQuantity] = useState(0);
+
+  const addToCart = () => {
+    if (quantity > 0) {
+      setCart((prev) => [
+        ...prev,
+        {
+          product: product.name,
+          quantity,
+          price: product.price,
+        },
+      ]);
+
+      setTotal((prevTotal) => prevTotal + product.price * quantity);
+    }
+  };
+
+  return (
+    <div className="grid-item" key={key}>
+      <div class="card">
+        <img
+          src={require(`../assets/image/${}`)}
+          alt={product.name}
+        />
+        <div class="card-body">
+          <h5 class="card-title">{product.name} Price:</h5>
+          <div class="quantity-container">
+            <label for="quantity">Quantity:</label>
+            <input
+              type="number"
+              id="quantity"
+              name="quantity"
+              value={quantity}
+              onChange={(e) => {
+                setQuantity(e.target.value);
+              }}
+            />
+          </div>
+          <button class="card-button" onClick={addToCart}>
+            Add to Cart
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
